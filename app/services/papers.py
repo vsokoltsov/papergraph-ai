30c2546028bf5abe3766ebc082adb009b8642aa9
Vadim Sokoltsov <vforvad@gmail.com>
2026-07-17 21:22:30 +0200

    Add papers service

---
+++ app/services/papers.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass
+from uuid import uuid5, NAMESPACE_URL
+from qdrant_client import models
+
+from app.clients.openalex import OpenAlexClient, OpenAlexArticle
+
+from app.repositories.vector import VectorRepository
+from app.errors import NoArticlesError
+
+def restore_abstract(index):
+    if not index:
+        return None
+
+    words = {}
+    for word, positions in index.items():
+        for position in positions:
+            words[position] = word
+
+    return " ".join(words[i] for i in sorted(words))
+
+@dataclass
+class PapersService:
+    openalex_client: OpenAlexClient
+    vector_repository: VectorRepository
+
+    def get_articles(self, query: str, limit: int = 20) -> list[OpenAlexArticle]:
+        articles = self.openalex_client.get_articles(
+            query=query,
+            limit=limit
+        )
+        
+        if len(articles) == 0: raise NoArticlesError
+
+        return articles
+
+    def insert_articles(self, articles: list[OpenAlexArticle]) -> None:
+        ids = []
+        vectors = []
+        payloads = []
+        model_name = "sentence-transformers/all-MiniLM-L6-v2"
+        for i, article in enumerate(articles):
+            abstract = restore_abstract(article.abstract_inverted_index)
+            text = f"{article.title}\n{abstract or ''}"
+            ids.append(str(uuid5(NAMESPACE_URL, article.id)))
+            vectors.append(
+                models.Document(
+                    text=text,
+                    model=model_name
+                )
+            )
+            payloads.append({
+                "openalex_id": article.id,
+                "doi": article.doi,
+                "title": article.title,
+                "abstract": abstract,
+                "publication_year": article.publication_year,
+                "publication_date": article.publication_date,
+                "cited_by_count": article.cited_by_count,
+                "language": article.language,
+                "type": article.type,
+                "open_access": article.open_access,
+                "primary_location": article.primary_location,
+                "best_oa_location": article.best_oa_location,
+            })
+
+        self.vector_repository.upload_papers(
+            ids=ids,
+            vectors=vectors,
+            payload=payloads,
+        )
+    
