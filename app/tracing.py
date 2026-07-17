045cdfd6b8a3ca9c2a579dd24fc530959763153b
Vadim Sokoltsov <vforvad@gmail.com>
2026-07-17 22:24:44 +0200

    Add tracing to app

---
+++ app/tracing.py
@@ -0,0 +1,21 @@
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from app.settings import Settings
+
+
+def configure_tracing(settings: Settings) -> None:
+    if not settings.OTEL_TRACING_ENABLED:
+        return
+
+    resource = Resource.create({"service.name": settings.OTEL_SERVICE_NAME})
+    provider = TracerProvider(resource=resource)
+    exporter = OTLPSpanExporter(endpoint=settings.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT)
+    provider.add_span_processor(BatchSpanProcessor(exporter))
+
+    trace.set_tracer_provider(provider)
+    HTTPXClientInstrumentor().instrument()
